8ebb7c9ff304f7857d2d214d7efd95a14b5b3c51
Javier Paniagua <panifran2000@gmail.com>
2022-04-22 23:49:28 +0200

    Add clean panel button

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 const container = document.querySelector('#container');
+const clean = document.querySelector('#clean');
 
 const numberSquares = 30;
 const squareSize = 600 / numberSquares;
+let div;
 
-function paintPanel(element) {
-  element.addEventListener('mouseenter', function () {
-    element.style.backgroundColor = 'red';
-  })
-}
 
 function createBlankSquare() {
-  const div = document.createElement('div');
+  div = document.createElement('div');
   div.style.width = squareSize + 'px';
   div.style.height = squareSize + 'px';
   div.style.borderRight = 'solid 1px black';
   div.style.borderBottom = 'solid 1px black';
   div.style.boxSizing = 'border-box';
   container.appendChild(div);
-  paintPanel(div);
 }
 
+function paintPanel(element) {
+  element.addEventListener('mouseenter', function () {
+    element.classList.add('red');
+  })
+}
+
+function cleanPanel(element) {
+  clean.addEventListener('click', function() {
+    element.classList.remove('red');
+  })
+}
 
 for (let i = 0; i < numberSquares * numberSquares; i++) {
   createBlankSquare();
+  paintPanel(div);
+  cleanPanel(div);
 }
-
