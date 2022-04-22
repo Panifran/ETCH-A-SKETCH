70c5a589e921ca207792e0575ed42e8671aca3e7
Javier Paniagua <panifran2000@gmail.com>
2022-04-22 16:08:04 +0200

    Add fill color without classes

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const container = document.querySelector('#container');
 
-const numberSquares = 50;
+const numberSquares = 30;
 const squareSize = 600 / numberSquares;
+
+function paintPanel(element) {
+  element.addEventListener('mouseenter', function () {
+    element.style.backgroundColor = 'red';
+  })
+}
 
 function createBlankSquare() {
   const div = document.createElement('div');
-  div.style.width = squareSize +'px';
-  div.style.height = squareSize +'px';
+  div.style.width = squareSize + 'px';
+  div.style.height = squareSize + 'px';
   div.style.borderRight = 'solid 1px black';
   div.style.borderBottom = 'solid 1px black';
   div.style.boxSizing = 'border-box';
-
   container.appendChild(div);
+  paintPanel(div);
 }
+
 
 for (let i = 0; i < numberSquares * numberSquares; i++) {
   createBlankSquare();
 }
+
